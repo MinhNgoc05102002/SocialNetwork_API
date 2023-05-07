07df704e016afbc7e5baa1417554cc324b1d7571
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-05-07 17:50:01 +0700

    fix login with fb

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -45,9 +45,9 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 
-    //app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseDeveloperExceptionPage();
+    //app.UseDeveloperExceptionPage();
     app.UseHsts();
 }
 
