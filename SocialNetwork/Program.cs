333d4fc70792f27da14ff84169678db31cec9ebb
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-05-07 17:17:58 +0700

    fix login with fb

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -42,14 +42,14 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-//app.UseDeveloperExceptionPage();
-app.UseHsts();
+if (!app.Environment.IsDevelopment())
+{
+
+    //app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseDeveloperExceptionPage();
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
