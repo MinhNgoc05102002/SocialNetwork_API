cfd2e527ee5f17ff390b8a5a2dea172e19df2b91
MinhNgoc05102002 <ngoc43552@gmail.com>
2023-05-07 17:10:50 +0700

    fix login with fb

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -48,7 +48,7 @@ var app = builder.Build();
 //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 //    app.UseHsts();
 //}
-app.UseDeveloperExceptionPage();
+//app.UseDeveloperExceptionPage();
 app.UseHsts();
 
 app.UseHttpsRedirection();
